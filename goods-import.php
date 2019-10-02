7313c6d1a7febb96677384a53dee328bd24118b3
Max Kucherov <deepdig@yandex.ru>
2019-10-02 06:43:44 +0000

    v1.7

--- goods-import.php
+++ goods-import.php
@@ -21,6 +21,21 @@ $modx->getService('error', 'error.modError');
 $modx->setLogLevel(modX::LOG_LEVEL_ERROR);
 $modx->setLogTarget(XPDO_CLI_MODE ? 'ECHO' : 'HTML');
 
+// Функция транслитерации
+function translit($s)
+{
+    $s = (string)$s; // преобразуем в строковое значение
+    $s = strip_tags($s); // убираем HTML-теги
+    $s = str_replace(array("\n", "\r"), " ", $s); // убираем перевод каретки
+    $s = preg_replace("/\s+/", ' ', $s); // удаляем повторяющие пробелы
+    $s = trim($s); // убираем пробелы в начале и конце строки
+    $s = function_exists('mb_strtolower') ? mb_strtolower($s) : strtolower($s); // переводим строку в нижний регистр (иногда надо задать локаль)
+    $s = strtr($s, array('а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'e', 'ж' => 'j', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shch', 'ы' => 'y', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya', 'ъ' => '', 'ь' => ''));
+    $s = preg_replace("/[^0-9a-z-_ ]/i", "", $s); // очищаем строку от недопустимых символов
+    $s = str_replace(" ", "-", $s); // заменяем пробелы знаком минус
+    return $s; // возвращаем результат
+}
+
 // Функция записи массива в лог
 function object2file($value, $filename)
 {
@@ -263,8 +278,8 @@ if (file_exists($category_file_original) and file_exists($products_file_original
                 }
 
                 if ($parentID) {
-                    echo "fdf";
-                    echo $productName; 
+                    //echo "fdf";
+                    //echo $productName; 
                     // обновляем поля товара
                     $product = $modx->getObject('modResource', $resourceID);
                     
@@ -328,7 +343,7 @@ if (file_exists($category_file_original) and file_exists($products_file_original
                         $productPrice = 0; // записываем цену 0
                     }
                     if ($publicProduct == 'False' and $productOrder == 1) { // Если у товара стоит галочка "Под заказ", цену делаем (для сортировки и фильтров) 999999
-                        $productPriceTv = 999999; // записываем цену 999999
+                        $productPrice = 999999; // записываем цену 999999
                     }
                     
                     $response = $modx->runProcessor('resource/create', array(
@@ -338,129 +353,69 @@ if (file_exists($category_file_original) and file_exists($products_file_original
                         'published' => $published, // опубликован
                         'parent' => $parentID,
                         'pagetitle' => $productName,
-                        //'alias' => $rows .'-'. translit($productName),
+                        'alias' => $rows .'-'. translit($productName),
                         'content' => $productContent,
                         // основные параметры msProduct
                         'class_key' => 'msProduct', // указываем что это товар minishop
                         'show_in_tree' => 0, // не показывать товар в древе ресурсов
-                        'article' => $productID, // Артикул
-                        'price' => $productPrice, // Цена
-                        'new' => $productNew, // Новый товар
-                        'popular' => $productTopSale, // Популярный товар
-                        'vendor' => $vendor, // производитель
+                        //'article' => $productID, // Артикул
+                        //'price' => $productPrice, // Цена
+                        //'new' => $productNew, // Новый товар
+                        //'popular' => $productTopSale, // Популярный товар
+                        //'vendor' => $vendor, // производитель
                         // устанавливаем TV поля
-                        'tv1' =>  $productID,          // TV - guidext
-                        'tv2' =>  $productParent,      // TV - guidextparent
-                        'tv5' =>  $productRemains,     // TV - product-remains
-                        'tv6' =>  $productPriceTv,       // TV - product-price
-                        'tv7' =>  $productNew,         // TV - product-new
-                        'tv8' =>  $productTopSale,     // TV - product-topSale
-                        'tv9' =>  $productProfitPrice, // TV - product-profitPrice
-                        'tv10' => $productOrder,       // TV - product-order
-                        'tv11' => $productIDAlso,      // TV - product-also
-                        'tv15' => $vendor,             // TV - filter-vendor
-                        'tv16' => $productDopCategory  // TV - product-dopCategory
+                        //'tv1' =>  $productID,          // TV - guidext
+                        //'tv2' =>  $productParent,      // TV - guidextparent
+                        //'tv5' =>  $productRemains,     // TV - product-remains
+                        //'tv6' =>  $productPriceTv,       // TV - product-price
+                        //'tv7' =>  $productNew,         // TV - product-new
+                        //'tv8' =>  $productTopSale,     // TV - product-topSale
+                        //'tv9' =>  $productProfitPrice, // TV - product-profitPrice
+                        //'tv10' => $productOrder,       // TV - product-order
+                        //'tv11' => $productIDAlso,      // TV - product-also
+                        //'tv15' => $vendor,             // TV - filter-vendor
+                        //'tv16' => $productDopCategory  // TV - product-dopCategory
                     ));
 
                     if ($response->isError()) {
-                        $errorArr = $modx->error->failure($response->getMessage());
+                        $errorArr = $response->getAllErrors();
                         object2file($errorArr, 'log.txt');
-                        file_put_contents('log.txt', PHP_EOL . serialize($errorArr), FILE_APPEND);
+                        file_put_contents('log.txt', PHP_EOL . $data . ' -- ' .serialize($errorArr), FILE_APPEND);
                         print_r($response->getAllErrors());
                     }
                     $modx->cacheManager->refresh(); // очищаем кэш
                     
-                }
-            }
-        }
-
-        $updated++;
-    }
-    
-    //цикл для записи фото товаров
-    /* Кучеров временно закрыл
-    while (($csv = fgetcsv($handle, 0, $delimeter, $delimeterEnd)) !== false) {
-
-        $rows++;
-
-        // определяем в переменные столбцы из файла    
-        $productName        = preg_replace( '/"([^"]*)"/', "«$1»", $csv[0] );  // Название ресурса (Names) - заменяем кавычки на << >>
-        $productContent     = preg_replace( '/"([^"]*)"/', "«$1»", $csv[1] );  // Описание товара (content) - заменяем кавычки на << >>
-        $productRemains     = $csv[2];                                    // Остатки (remains)
-        $productPrice       = preg_replace("/[^x\d|*\.]/", "", $csv[3]);  // цена (price)  удаляем пробелы
-        $productNew         = $csv[4];                                    // для формирования блока "Новинки" (new)
-        $productTopSale     = $csv[5];                                    // для формирования блока "Лидеры продаж" (topSale)
-        $productProfitPrice = $csv[6];                                    // для формирования блока "Выгодная цена" (profit_price)
-        $productOrder       = $csv[7];                                    // для индентификации товара "Под заказ" (order)
-        $productID          = $csv[8];                                    // идентификатор ресурса (GUIDExt)
-        $productParent      = $csv[9];                                    // родительский ресурс (GUIDExtParent)
-        $productDopCategory = $csv[10];                                   // доп.категории товара (GUIDExtParents)
-        $productIDAlso      = $csv[11];                                   // список UID товаров через запятую для формирования вкладки "Вам могут понадобиться" (GUIDExt_also)
-        $publicProduct      = $csv[12];                                   // столбец Delete_Tovar
-        $vendor             = $csv[13];                                   // фильтр по производителю (Filter1)
-        $filter2            = $csv[14];                                   // (Filter2)
-        $filter3            = $csv[15];                                   // (Filter3)
-        $filter4            = $csv[16];                                   // (Filter4)
-        $filter5            = $csv[17];                                   // (Filter5)
-
-        
-
-        if ($productName != 'Names' and $productName != 'Стройматериалы') {
-
-            //echo $productName . '<br>';
-
-            // проверяем наличие ресурса в каталоге
-            $sql = 'SELECT * FROM `modx_sablsite_content` WHERE pagetitle="' . $productName . '"';
-            $statement = $modx->query($sql);
-            if ($statement) {
-                $info = $statement->fetchAll(PDO::FETCH_ASSOC);
-                $resourceID = $info[0]['id'];
-            }
-
-
-            if (!empty($resourceID)) { // если товар уже имеется в каталоге, обновляем поля
-
-                
-
-                if ($parentID) {
-                    // обновляем поля товара
-                    $product = $modx->getObject('modResource', $resourceID);
-                    
-
-                    //--- Обновляем галерею фото ----
-                    $dir_imgs = $dir_image . $productID . '/';
-                    
-                    if (is_dir($dir_imgs)) {// проверяем наличие папки с фото
-                        foreach (array_diff(scandir($dir_imgs), array('..', '.')) as $file_img) {
-                            $file_path = $dir_imgs . $file_img; // полный путь к файлу
-    
-                            $data = [
-                                'id' => $resourceID, // id - ресурса
-                                'file' => $file_path, // путь к картинке
-                            ];
-                            // Вызов процессора загрузки
-                            
-                            
-                            $responseImg = $modx->runProcessor('gallery/upload', $data, [
-                                'processors_path' => MODX_CORE_PATH . 'components/minishop2/processors/mgr/',
-                            ]);
-    
-                            // Вывод результата работы процессора
-                            if ($responseImg->isError()) {
-                                print_r($responseImg->getAllErrors());
-                            }
-                            
-                        }
+                    // получаем id созданного ресурса и записываем в него тв-поля
+                    $newId = $response->response['object']['id'];
+                    if ($newId) {
+                        $pageNew = $modx->getObject('msProduct', $newId);
+                        // основные параметры msProduct
+                        $pageNew->set('alias', 'products/' . $newId . '-'. translit($productName));
+                        $pageNew->set('article', $productID);
+                        $pageNew->set('price', $productPrice);
+                        $pageNew->set('new', $productNew);
+                        $pageNew->set('popular', $productTopSale);
+                        $pageNew->set('vendor', $vendor);
+                        // запись в доп.поле
+                        $pageNew->setTVValue('guidext', $productID);
+                        $pageNew->setTVValue('guidextparent', $productParent);
+                        $pageNew->setTVValue('product-remains', $productRemains);
+                        $pageNew->setTVValue('product-price', $productPrice);
+                        $pageNew->setTVValue('product-new', $productNew);
+                        $pageNew->setTVValue('product-topSale', $productTopSale);
+                        $pageNew->setTVValue('product-profitPrice', $productProfitPrice);
+                        $pageNew->setTVValue('product-order', $productOrder);
+                        $pageNew->setTVValue('product-also', $productIDAlso);
+                        $pageNew->setTVValue('filter-vendor', $vendor);
+                        $pageNew->setTVValue('product-dopCategory', $productDopCategory);
+                        $pageNew->save();
                     }
                 }
-            } else {
-                
             }
         }
 
         $updated++;
     }
-    */
     
     fclose($handle);
     unlink($category_file_original); // удаляем файл импорта
